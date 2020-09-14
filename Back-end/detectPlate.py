6af1c7e2a0b978b3695e3ee470867e6b456102ca
0xMousa <0xmousa@gmail.com>
2020-09-14 19:01:26 +0300

    Fix all bugs in detectPlate class

--- Back-end/detectPlate.py
+++ Back-end/detectPlate.py
@@ -7,6 +7,7 @@ import numpy as np
 from local_utils import detect_lp
 from detectCars import tf_config
 
+
 class detectPlate:
     def __init__(self):
         path = os.path.join(os.path.abspath(".") , "weightFiles/wpod-net")
@@ -23,7 +24,7 @@ class detectPlate:
 
     def __changeImage(self , image):
         image = cv2.cvtColor(image , cv2.COLOR_BGR2RGB)
-        return image / 255
+        return (image / 255)
     
     def __getPlate(self,image, Dmax=608, Dmin = 608):
         with self.graph.as_default():
@@ -35,10 +36,11 @@ class detectPlate:
             _ , LpImg, _, cor = detect_lp(self.model, car, bound_dim, lp_threshold=0.5)
             return car, LpImg, cor
     
-    def __sort_contours(self,cnts,reverse = False):
+    def __sortContours(self,cnts,reverse = False):
         i = 0
         boundingBoxes = [cv2.boundingRect(c) for c in cnts]
-        (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),key=lambda b: b[1][i], reverse=reverse))
+        (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+                                        key=lambda b: b[1][i], reverse=reverse))
         return cnts
     
     def __imageX(self , plate):
@@ -47,60 +49,90 @@ class detectPlate:
         blur = cv2.GaussianBlur(gray,(7,7),0)
         binary = cv2.threshold(blur, 180, 255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
         kernel3 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-        thre_mor = cv2.morphologyEx(binary, cv2.MORPH_DILATE, kernel3)
-        return plate_image,binary,thre_mor
+        threMor = cv2.morphologyEx(binary, cv2.MORPH_DILATE, kernel3)
+        return plate_image,binary,threMor
     
     def getRectangularPlate(self,image):
         car, plate, dim = self.__getPlate(image)
         plate = plate[0]
-        return self.__getChars(plate,1,2,4.5)
-        
+        '''
+        plt.imshow(plate)
+        plt.show()
+        '''
+        return self.__getChars(plate, 1 , 2 , 4.5 )
 
     def getSquarePlate(self,image):
         car, plate, dim = self.__getPlate(image)
+        h = plate[0].shape[0]
         plate = np.concatenate((plate[0][0:int(h*0.55)],plate[0][int(h*0.45):h]) ,axis = 1 )#To make it rectangular
+        '''
+        plt.imshow(plate)
+        plt.show()
+        '''
         return self.__getChars(plate ,1.5 ,2.5,5.5)
         
     def __getChars(self , plate , minRatio , maxRatio , oneRatio):
         plateImage,binary,threMor = self.__imageX(plate)
+        '''
+        plt.imshow(threMor)
+        plt.show()
+        '''
         cont, _  = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cropCharacters = []
         onePostions = []
         digit_w, digit_h = 30, 60
-
-        for i,c in enumerate(self.__sort_contours(cont)):
+        i=0
+        for c in self.__sortContours(cont):
             (x, y, w, h) = cv2.boundingRect(c)
             charRatio = h/w
             if minRatio<=charRatio<=maxRatio and h/plateImage.shape[0]>=0.5:
-                    currNum = threMor[y:y+h,x:x+w]
-                    currNum = cv2.resize(currNum, dsize=(digit_w, digit_h))
-                    _, currNum = cv2.threshold(currNum, 220, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-                    cropCharacters.append(currNum)
-            elif oneRatio<=charRatio<=oneRatio+1 and h/plateImage.shape[0]>=0.5:
+                currNum = threMor[y:y+h,x:x+w]
+                currNum = cv2.resize(currNum, dsize=(digit_w, digit_h))
+                _, currNum = cv2.threshold(currNum, 220, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                cropCharacters.append(currNum)
+                i+=1
+            elif oneRatio<=charRatio<=(oneRatio+1) and h/plateImage.shape[0]>=0.5:
                 currNum = threMor[y:y+h,x:x+w]
                 currNum = cv2.resize(currNum, dsize=(digit_w, digit_h))
                 _, currNum = cv2.threshold(currNum, 220, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
                 cropCharacters.append(currNum)
                 onePostions.append(i)
-            
-            # the number one on jordan's plate different , it's like | not 1
-            # here I fix this bug   
-            if len(onePostions):
-                pass
+                i+=1
 
+        # the number one on jordan's plate different , it's like | not 1
+        # here I fix this bug        
+        if len(onePostions):
+            noOneImage = os.path.join(os.path.abspath(".") ,"images/one.jpg")
+            image = cv2.imread(noOneImage)
+            image = self.__changeImage(image)
+            plateImage,binary,threMor = self.__imageX(image)
+            cont, _  = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            numOneImage = []
+            for c in self.__sortContours(cont):
+                (x, y, w, h) = cv2.boundingRect(c)
+                if h/plateImage.shape[0] >= 0.7:
+                    currNum = threMor[y:y+h,x:x+w]
+                    currNum = cv2.resize(currNum, dsize=(digit_w, digit_h))
+                    _, currNum = cv2.threshold(currNum, 220, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                    numOneImage= currNum
 
+            for i in onePostions:
+                cropCharacters[i] = numOneImage
+        
         return cropCharacters #return crop characters from thre_mor image
     
     def getPlateType(self , image):
-        car, plate, dim = self.__getPlate(image)
-        if len(plate):
-            h = plate[0].shape[0]
-            w = plate[0].shape[1]
-            if 90<=h<=130 and 420<=w<=520:
-                plateType = "Rectangular"
-            elif 170<=h<=240 and 250<=w<=310:
-                plateType = "Square"
-        else:
+        try:
+            car, plate, dim = self.__getPlate(image)
+            if len(plate):
+                h = plate[0].shape[0]
+                w = plate[0].shape[1]
+                if 90<=h<=130 and 420<=w<=520:
+                    plateType = "Rectangular"
+                elif 170<=h<=240 and 250<=w<=310:
+                    plateType = "Square"
+            else:
+                plateType = "Didn't find a plate"
+        except:
             plateType = "Didn't find a plate"
-        
         return plateType
--- Back-end/test.py
+++ Back-end/test.py
@@ -5,13 +5,42 @@ import cv2
 import matplotlib.pyplot as plt
 from detectPlate import detectPlate
 
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from local_utils import detect_lp
+from os.path import splitext,basename
+from keras.models import model_from_json
+from keras.preprocessing.image import load_img, img_to_array
+from keras.applications.mobilenet_v2 import preprocess_input
+from sklearn.preprocessing import LabelEncoder
+import glob
+
 
 root = os.path.abspath(".")
-image = os.path.join(root , "images/Jordan2.jpeg")
+image = os.path.join(root , "images/IMG_3816.JPG")
  
 plate = detectPlate()
 
 i = cv2.imread(image)
-print(plate.getPlateType(i))
-plt.imshow(i)
+plateType = plate.getPlateType(i)
+print(plateType)
+if plateType == "Rectangular":
+    c = plate.getRectangularPlate(i)
+elif plateType == "Square":
+    c = plate.getSquarePlate(i)
+elif plateType == "Didn't find a plate":
+    pass
+
+fig = plt.figure(figsize=(14,4))
+grid = gridspec.GridSpec(ncols=len(c),nrows=1,figure=fig)
+try:
+    for i in range(len(c)):
+        fig.add_subplot(grid[i])
+        plt.axis(False)
+        plt.imshow(c[i],cmap="gray")
+except:
+    print(i)
+
 plt.show()
